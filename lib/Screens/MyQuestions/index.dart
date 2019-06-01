1faba3bd249593176086938e0bcd00c0694227de
Suraj <suraj@sahusoft.com>
2019-06-01 15:27:40 +0530

    Added my question

--- lib/Screens/MyQuestions/index.dart
+++ lib/Screens/MyQuestions/index.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:perffers/Screens/QuestionChat/index.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyQuestions extends StatelessWidget {
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +18,7 @@ class MyQuestions extends StatelessWidget {
                   children: <Widget>[
                     const ListTile(
                       leading: Icon(Icons.lightbulb_outline),
-                      title: Text('Some random question'),
+                      title: Text('Your questions'),
                     ),
                   ],
                 ),
@@ -29,9 +28,9 @@ class MyQuestions extends StatelessWidget {
                     padding: const EdgeInsets.all(10.0),
                     child: StreamBuilder<QuerySnapshot>(
                       stream: Firestore.instance
-                          .collection(
-                              'questions/k5UQFYaq4JV3ggJhQI1a/question_messages')
-                          .orderBy("created_at")
+                          .collection('questions')
+                          .where("user_id",
+                              isEqualTo: "V9BPdjICY0Wr0lPnkbZy1a6x4kH3")
                           .snapshots(),
                       builder: (BuildContext context,
                           AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -47,17 +46,19 @@ class MyQuestions extends StatelessWidget {
                                 return new Container(
                                   margin: const EdgeInsets.all(5.0),
                                   child: new Column(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: <Widget>[
-                                      new Text("User",
-                                          style: new TextStyle(
-                                              fontSize: 14.0,
-                                              color: Colors.black,
-                                              fontWeight: FontWeight.bold)),
-                                      new Container(
-                                        margin: const EdgeInsets.only(top: 5.0),
-                                        child: new Text(document['message']),
-                                      ),
+                                      Card(
+                                          child: ListTile(
+                                              title: Text(document['caption']),
+                                              onTap: () {
+                                                Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            new QuestionChat()));
+                                              })),
                                     ],
                                   ),
                                 );
@@ -67,50 +68,8 @@ class MyQuestions extends StatelessWidget {
                       },
                     )),
               ),
-              new Divider(height: 1.0),
-              new Container(
-                decoration:
-                    new BoxDecoration(color: Theme.of(context).cardColor),
-                child: new Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: new Row(
-                    children: <Widget>[
-                      new Flexible(
-                          child: new TextFormField(
-                              style: TextStyle(
-                                  color: Color.fromRGBO(00, 00, 00, 1),
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.normal),
-                              key: key,
-                              obscureText: false,
-                              keyboardType: TextInputType.text,
-                              onSaved: (String email) {
-                                // user.email = email;
-                              },
-                              decoration: new InputDecoration(
-                                hintText: "Type your answer",
-                              ))),
-                      new Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                          child: new IconButton(
-                              icon: new Icon(Icons.send),
-                              onPressed: () => {
-                                    Navigator.of(context)
-                                        .pushNamed('/askQuestion')
-                                  })),
-                    ],
-                  ),
-                ),
-              ),
             ],
           ),
-          decoration: Theme.of(context).platform == TargetPlatform.iOS
-              ? new BoxDecoration(
-                  border: new Border(
-                      top: new BorderSide(
-                  color: Colors.grey[200],
-                )))
-              : null,
         ));
   }
 }
