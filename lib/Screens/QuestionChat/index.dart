5fa1719275cdadd2ba0efae64c78120754d186e5
Rishab Jain <rishab1065@gmail.com>
2019-06-01 11:28:49 +0530

    basic question chat screen

--- lib/Screens/QuestionChat/index.dart
+++ lib/Screens/QuestionChat/index.dart
@@ -5,17 +5,50 @@ class QuestionChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('First Route'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            Navigator.of(context).pushNamed('/questionChat');
-          },
+        appBar: AppBar(
+          title: Text('Question'),
         ),
-      ),
-    );
+        body: new Container(
+            margin: new EdgeInsets.all(16),
+            child: new Column(
+              children: <Widget>[
+                Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.lightbulb_outline),
+                        title: Text('The Enchanted Nightingale'),
+                        subtitle: Text('Some random question'),
+                      ),
+                    ],
+                  ),
+                ),
+                // new Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //   children: <Widget>[
+                Flexible(
+                  flex: 1,
+                  child: new Container(),
+                ),
+
+                new Container(
+                    height: 70.0,
+                    child: new TextFormField(
+                        style: TextStyle(
+                            color: Color.fromRGBO(00, 00, 00, 1),
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.normal),
+                        key: key,
+                        obscureText: false,
+                        keyboardType: TextInputType.text,
+                        onSaved: (String email) {
+                          // user.email = email;
+                        },
+                        decoration: new InputDecoration(
+                          hintText: "Type your answer",
+                        ))),
+              ],
+            )));
   }
 }
