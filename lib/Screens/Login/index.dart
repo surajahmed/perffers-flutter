273fd3c1c1254e32fcc6e5ca4a2952c1fdddaf70
Rishab Jain <rishab1065@gmail.com>
2019-06-01 13:35:08 +0530

    added home screen

--- lib/Screens/Login/index.dart
+++ lib/Screens/Login/index.dart
@@ -10,26 +10,6 @@ class Login extends StatefulWidget {
 class _LoginScreenState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    // Private methods within build method help us to
-    // organize our code and recognize structure of widget
-    // that we're building:
-    // BoxDecoration _buildBackground() {
-    //   return BoxDecoration(
-    //     image: DecorationImage(
-    //       // image: AssetImage("assets/brooke-lark-385507-unsplash.jpg"),
-    //       fit: BoxFit.cover,
-    //     ),
-    //   );
-    // }
-
-    Text _buildText() {
-      return Text(
-        'Recipes',
-        style: Theme.of(context).textTheme.headline,
-        textAlign: TextAlign.center,
-      );
-    }
-
     return Scaffold(
       body: Container(
         // decoration: _buildBackground(),
@@ -37,7 +17,11 @@ class _LoginScreenState extends State<Login> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              _buildText(),
+              Text(
+                'Tech Pair',
+                style: Theme.of(context).textTheme.headline,
+                textAlign: TextAlign.center,
+              ),
               SizedBox(height: 50.0),
               GoogleSignInButton(
                 // Passing function callback as constructor argument:
--- lib/main.dart
+++ lib/main.dart
@@ -3,90 +3,16 @@ import 'package:perffers/Screens/QuestionChat/index.dart';
 import 'package:perffers/Screens/AskQuestion/index.dart';
 import 'package:perffers/Screens/Question/index.dart';
 import 'package:perffers/Screens/Login/index.dart';
+import 'package:perffers/Screens/Home/index.dart';
 
 void main() => runApp(MaterialApp(
       title: 'Navigation Basics',
-      home: AskQuestion(),
+      home: Login(),
       routes: <String, WidgetBuilder>{
+        '/home': (BuildContext context) => new Home(),
         '/askQuestion': (BuildContext context) => new AskQuestion(),
         '/questionChat': (BuildContext context) => new QuestionChat(),
         '/questions': (BuildContext context) => new QuestionScreen(),
         '/login': (BuildContext context) => new Login(),
       },
     ));
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('First Route'),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                accountName: Text("Ashish Rawat"),
-                accountEmail: Text("ashishrawat2911@gmail.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor:
-                      Theme.of(context).platform == TargetPlatform.iOS
-                          ? Colors.blue
-                          : Colors.white,
-                  child: Text(
-                    "A",
-                    style: TextStyle(fontSize: 40.0),
-                  ),
-                ),
-              ),
-              ListTile(
-                title: Text("Answer Questions"),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (BuildContext context) => QuestionScreen()));
-                },
-              ),
-              ListTile(
-                title: Text("Ask Questions"),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          QuestionScreen())); // TODO: add ask question screen
-                },
-              ),
-              ListTile(
-                title: Text("Logout"),
-                onTap: () {
-                  // TODO: logout here
-                },
-              ),
-            ],
-          ),
-        ),
-        body: Row(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('Open route'),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/questionChat');
-              },
-            ),
-            RaisedButton(
-              child: Text('Questions'),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/questions');
-              },
-            ),
-            RaisedButton(
-              child: Text('Login'),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/login');
-              },
-            ),
-          ],
-        ));
-  }
-}
