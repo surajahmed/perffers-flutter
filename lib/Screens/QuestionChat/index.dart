4f64bfae9824f1c671c2460763d4470a728cf1c5
Rishab Jain <rishab1065@gmail.com>
2019-06-01 11:45:16 +0530

    ui

--- lib/Screens/QuestionChat/index.dart
+++ lib/Screens/QuestionChat/index.dart
@@ -9,46 +9,77 @@ class QuestionChat extends StatelessWidget {
           title: Text('Question'),
         ),
         body: new Container(
-            margin: new EdgeInsets.all(16),
-            child: new Column(
-              children: <Widget>[
-                Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
+          child: new Column(
+            children: <Widget>[
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(Icons.lightbulb_outline),
+                      title: Text('The Enchanted Nightingale'),
+                      subtitle: Text('Some random question'),
+                    ),
+                  ],
+                ),
+              ),
+              new Flexible(
+                child: Container(),
+                // new FirebaseAnimatedList(
+                //   query: reference,
+                //   padding: const EdgeInsets.all(8.0),
+                //   reverse: true,
+                //   sort: (a, b) => b.key.compareTo(a.key),
+                //   //comparing timestamp of messages to check which one would appear first
+                //   itemBuilder: (_, DataSnapshot messageSnapshot,
+                //       Animation<double> animation) {
+                //     return new ChatMessageListItem(
+                //       messageSnapshot: messageSnapshot,
+                //       animation: animation,
+                //     );
+                //   },
+                // ),
+              ),
+              new Divider(height: 1.0),
+              new Container(
+                decoration:
+                    new BoxDecoration(color: Theme.of(context).cardColor),
+                child: new Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: new Row(
                     children: <Widget>[
-                      const ListTile(
-                        leading: Icon(Icons.lightbulb_outline),
-                        title: Text('The Enchanted Nightingale'),
-                        subtitle: Text('Some random question'),
-                      ),
+                      new Flexible(
+                          child: new TextFormField(
+                              style: TextStyle(
+                                  color: Color.fromRGBO(00, 00, 00, 1),
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.normal),
+                              key: key,
+                              obscureText: false,
+                              keyboardType: TextInputType.text,
+                              onSaved: (String email) {
+                                // user.email = email;
+                              },
+                              decoration: new InputDecoration(
+                                hintText: "Type your answer",
+                              ))),
+                      new Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                          child: new IconButton(
+                              icon: new Icon(Icons.send), onPressed: () => {})),
                     ],
                   ),
                 ),
-                // new Row(
-                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //   children: <Widget>[
-                Flexible(
-                  flex: 1,
-                  child: new Container(),
-                ),
-
-                new Container(
-                    height: 70.0,
-                    child: new TextFormField(
-                        style: TextStyle(
-                            color: Color.fromRGBO(00, 00, 00, 1),
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.normal),
-                        key: key,
-                        obscureText: false,
-                        keyboardType: TextInputType.text,
-                        onSaved: (String email) {
-                          // user.email = email;
-                        },
-                        decoration: new InputDecoration(
-                          hintText: "Type your answer",
-                        ))),
-              ],
-            )));
+              ),
+            ],
+          ),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+                  border: new Border(
+                      top: new BorderSide(
+                  color: Colors.grey[200],
+                )))
+              : null,
+        ));
   }
 }
