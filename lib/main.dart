6903cf9b1a3e56730fa74ca67b5636b8be053f0c
Suraj <suraj@sahusoft.com>
2019-06-01 12:17:43 +0530

    Added drawer

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:perffers/Screens/Question/index.dart';
 
 void main() => runApp(MaterialApp(
       title: 'Navigation Basics',
-      home: QuestionScreen(),
+      home: MyApp(),
       routes: <String, WidgetBuilder>{
         '/questionChat': (BuildContext context) => new QuestionChat(),
         '/questions': (BuildContext context) => new QuestionScreen(),
@@ -18,6 +18,49 @@ class MyApp extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text('First Route'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text("Ashish Rawat"),
+                accountEmail: Text("ashishrawat2911@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor:
+                      Theme.of(context).platform == TargetPlatform.iOS
+                          ? Colors.blue
+                          : Colors.white,
+                  child: Text(
+                    "A",
+                    style: TextStyle(fontSize: 40.0),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text("Answer Questions"),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) => QuestionScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Ask Questions"),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          QuestionScreen())); // TODO: add ask question screen
+                },
+              ),
+              ListTile(
+                title: Text("Logout"),
+                onTap: () {
+                  // TODO: logout here
+                },
+              ),
+            ],
+          ),
         ),
         body: Row(
           children: <Widget>[
